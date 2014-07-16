7181b0b8efd61f655549486b01014abae2e49bb6
Hernan Gonzales <hernan.gonzales@dominionenterprises.com>
2014-07-16 13:40:32 -0400

    added search scenario

--- src/com/commercialtrucktrader/home/TestSearchTrucks.java
+++ src/com/commercialtrucktrader/home/TestSearchTrucks.java
@@ -1,5 +1,6 @@
 package com.commercialtrucktrader.home;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
@@ -18,27 +19,77 @@ import com.dominionenterprises.selenium.util.TestCaseHelper;
  */
 public class TestSearchTrucks extends TestCaseHelper{
 
+	  private ArrayList<String> xpaths;
+		
 	  @Before
 	  public void setUp() throws Exception {
 		  Boolean good = super.init(TestSearchTrucks.class.getName());
 			if(!good)
 				throw new Exception();
+			
+			xpaths = new ArrayList<String>();
+			xpaths.add("//h3/a");
+			xpaths.add("//div[4]/div/h3/a");
+			xpaths.add("//div[5]/div/h3/a");
+			xpaths.add("//div[7]/div/h3/a");
+			xpaths.add("//div[8]/div/h3/a");
+			xpaths.add("//div[9]/div/h3/a");
+			//xpaths.add("//div[10]/div/h3/a");
+			xpaths.add("//div[11]/div/h3/a");
+			xpaths.add("//div[12]/div/h3/a");
+			xpaths.add("//div[13]/div/h3/a");
+			xpaths.add("//div[14]/div/h3/a");
+			xpaths.add("//div[15]/div/h3/a");
+			xpaths.add("//div[16]/div/h3/a");//add
+			//xpaths.add("//div[17]/div/h3/a");
+			xpaths.add("//div[18]/div/h3/a");
+			xpaths.add("//div[19]/div/h3/a");
+			xpaths.add("//div[20]/div/h3/a");
+			xpaths.add("//div[21]/div/h3/a");
+			xpaths.add("//div[22]/div/h3/a");
+			xpaths.add("//div[23]/div/h3/a");
+			xpaths.add("//div[24]/div/h3/a");		
+			//xpaths.add("//div[25]/div/h3/a");
+			//xpaths.add("//div[26]/div/h3/a");
+			//xpaths.add("//div[27]/div/h3/a");
+			xpaths.add("//div[28]/div/h3/a");
+			xpaths.add("//div[29]/div/h3/a");
+			xpaths.add("//div[30]/div/h3/a");//add
+			xpaths.add("//div[31]/div/h3/a");//add
+			xpaths.add("//div[32]/div/h3/a");//add
+			xpaths.add("//div[33]/div/h3/a");//add
+			
 	  }
 	
 	  @Test
 	  public void testHomeSearchTrucks() throws Exception {
+		  
+		  String make = "DODGE";
 		  
 		  //click("cssSelector","img[alt=\"Go To Commercial Truck Home Page\"]");
 		  click("linkText","HOME");
 		  //moveToElement("linkText","» Search Trucks");
 		  click("linkText","» Search Trucks");
 
-		  //element.put("Dodge/Ram","cssSelector");
-		  //element.put("Dodge / Ram","cssSelector");
-		  //element.put("Dodge.*.Ram","cssSelector");
-		  //doVerifyTextNotPresentList(element, "BODY");
-
+		  element.put("SEARCH TRUCK LISTINGS","xpath");
+		  doVerifyTextPresentList(element, "//h2");
 		  
+		  click("id","newOrUsed");
+		  select("id","lYear","2001");
+		  select("id","hYear","2009");
+		  select("id","makesDrop",make);
+		  input("id","zipcode","23510");
+		  click("cssSelector","a.sGo");
+		  
+		  element.clear();
+		  element.put("Dodge Trucks for Sale near Norfolk, Virginia","xpath");
+		  doVerifyTextPresentList(element, "//div[@id='topic_container']/div/h1");
+		  
+		  element.clear();
+		  element.put("^[\\s\\S]*Dodge results found[\\s\\S]*$","xpath");
+		  doVerifyTextPresentList(element, "//div[@id='pagineationSort_container']/h2");
+		  
+		  verifyResults(make, xpaths);
 	  }
 	
 	  @After
@@ -50,6 +101,12 @@ public class TestSearchTrucks extends TestCaseHelper{
 		  }
 	  }
 
-  
+	  private void verifyResults(String mk, ArrayList<String> xps){
+		  element.clear(); 
+		  element.put("^[\\s\\S]*200[1-9](?i:.*"+mk+"*)[\\s\\S]*$","xpath");	  
+		  for(String xp : xps){ 
+			  doVerifyTextPresentList(element, xp); //Verify all results header/title
+		  }
+	  }  
 	
 }
